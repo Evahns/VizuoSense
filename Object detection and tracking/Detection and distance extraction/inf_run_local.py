767a82af3f7384bd8783f75a78520cbad30e7b15
pytoni <116155840+1997MarsRover@users.noreply.github.com>
2023-11-02 09:50:04 +0300

    Update inf_run_local.py

--- Object detection and tracking/Detection and distance extraction/inf_run_local.py
+++ Object detection and tracking/Detection and distance extraction/inf_run_local.py
@@ -1,14 +1,70 @@
+from collections import defaultdict
 from time import time
 import cv2
-import math
 import os
 import torch
 import torchvision.transforms as T
 from PIL import Image
 import numpy as np
 from ultralytics import YOLO
-import pandas as pd 
 import json
+
+class ObjectDetector:
+    def __init__(self, weights_path, classNames):
+        self.model = YOLO(weights_path)
+        self.classNames = classNames
+
+    def detect_objects(self, frame):
+        results = self.model(frame)
+        detected_objects = []
+        for r in results:
+            boxes = r.boxes
+            for box in boxes:
+                x1, y1, x2, y2 = box.xyxy[0]
+                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+                w, h = x2 - x1, y2 - y1
+                conf = round(box.conf[0].item(), 2)
+                cls = int(box.cls[0])
+                object_class = self.classNames[cls] if 0 <= cls < len(self.classNames) else "Unknown"
+                detected_objects.append({
+                    'x1': x1, 'y1': y1, 'x2': x2, 'y2': y2,
+                    'conf': conf, 'class': object_class,
+                })
+        return detected_objects
+
+class DepthEstimator:
+    def __init__(self):
+        self.midas = torch.hub.load("intel-isl/MiDaS", "MiDaS_small").eval()
+
+    def calculate_depth_map(self, frame):
+        transform = T.Compose([T.Resize(384), T.ToTensor()])
+        input_image = transform(Image.fromarray(frame)).unsqueeze(0)
+        with torch.no_grad():
+            prediction = self.midas(input_image)
+        depth_map = prediction.squeeze().cpu().numpy()
+        depth_map = cv2.normalize(depth_map, None, 0, 255, cv2.NORM_MINMAX)
+        scaling_factor = 0.01
+        depth_map = depth_map * scaling_factor
+        return depth_map
+
+class FrameProcessor:
+    def __init__(self, camera_center_x, camera_center_y, focal_length, object_detector, depth_estimator):
+        self.camera_center_x = camera_center_x
+        self.camera_center_y = camera_center_y
+        self.focal_length = focal_length
+        self.object_detector = object_detector
+        self.depth_estimator = depth_estimator
+
+    def process_frame(self, frame):
+        depth_map = self.depth_estimator.calculate_depth_map(frame)
+        detected_objects = self.object_detector.detect_objects(frame)
+        processed_frame = self.draw_objects(frame, detected_objects, depth_map)
+        return processed_frame
+
+    def draw_objects(self, frame, detected_objects, depth_map):
+        # Drawing code for objects and depth lines
+        # ... (your existing code for drawing objects and depth lines)
+        return frame
 
 class NumpyEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -18,209 +74,56 @@ class NumpyEncoder(json.JSONEncoder):
             return float(obj)  # Convert np.float32 to regular float
         return super(NumpyEncoder, self).default(obj)
 
-# Specify the directory where you want to save the frames
-output_directory ="C:\\Users\\Admin\\Pictures\\Depth"
+class FrameAnalyzer:
+    def __init__(self, output_directory, processing_interval, max_frames_to_process):
+        self.output_directory = output_directory
+        self.processing_interval = processing_interval
+        self.max_frames_to_process = max_frames_to_process
+        self.start_time = time()
+        self.frame_count = 0
+        self.processed_frames = []
 
-# Create the output directory if it doesn't exist
-if not os.path.exists(output_directory):
-    os.makedirs(output_directory)
+    def analyze_frames(self, cap, frame_processor):
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                break
+            self.frame_count += 1
+            processed_frame = frame_processor.process_frame(frame)
+            output_image_path = os.path.join(self.output_directory, f"frame_{self.frame_count}.jpg")
+            cv2.imwrite(output_image_path, processed_frame)
+            self.save_json_data()
+            if self.should_break():
+                break
 
-# Load the YOLO model
-model = YOLO("C://Users//Admin//Downloads//yolo-weights//yolov8l.pt")
+    def save_json_data(self):
+        elapsed_time = time() - self.start_time
+        if elapsed_time >= self.processing_interval:
+            json_data = json.dumps(self.processed_frames, indent=4, cls=NumpyEncoder)
+            output_json_path = f"C:/Users/Admin/Documents/processed_frames_{self.frame_count}.json"
+            with open(output_json_path, "w") as json_file:
+                json_file.write(json_data)
+            print(f"Success! JSON data saved to {output_json_path}")
+            self.processed_frames = []
+            self.start_time = time()
 
-# Load the MiDaS model from PyTorch Hub
-midas = torch.hub.load("intel-isl/MiDaS", "MiDaS_small").eval()
+    def should_break(self):
+        return self.max_frames_to_process and self.frame_count >= self.max_frames_to_process
+    
+def main():
+    output_directory = "C:\\Users\\Admin\\Pictures\\Depth"
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
 
-# Customized class names for campus navigation
-classNames = ["person", "bicycle", "car", "motorbike", "bus", "truck", "tie"]
+    object_detector = ObjectDetector("C://Users//Admin//Downloads//yolo-weights//yolov8l.pt", ["person", "bicycle", "car", "motorbike", "bus", "truck", "tie"])
+    depth_estimator = DepthEstimator()
+    camera_center_x, camera_center_y, focal_length = 320, 240, 3.0
 
-# Function to calculate depth map using the MiDaS model
-def calculate_depth_map(frame):
-    # Resize the input image to the required size
-    transform = T.Compose([T.Resize(384), T.ToTensor()])
-    input_image = transform(Image.fromarray(frame)).unsqueeze(0)
+    frame_processor = FrameProcessor(camera_center_x, camera_center_y, focal_length, object_detector, depth_estimator)
 
-    with torch.no_grad():
-        prediction = midas(input_image)
-
-    depth_map = prediction.squeeze().cpu().numpy()
-    depth_map = cv2.normalize(depth_map, None, 0, 255, cv2.NORM_MINMAX)
-
-    # Adjust the scaling factor based on the MiDaS model's output
-    scaling_factor = 0.01
-    depth_map = depth_map * scaling_factor
-
-    return depth_map
-
-def calculate_distance(x1, y1, x2, y2, depth_map):
-    # Define the region of interest within the bounding box
-    roi = depth_map[y1:y2, x1:x2]
-
-    # Calculate the weighted average depth within the region
-    y, x = np.indices(roi.shape)
-    total_depth = np.sum(roi)
-    weighted_x = np.sum(x * roi)
-    weighted_y = np.sum(y * roi)
-
-    if total_depth > 0:
-        center_x = x1 + weighted_x / total_depth
-        center_y = y1 + weighted_y / total_depth
-        distance = roi[int(center_y - y1), int(center_x - x1)]
-        return distance
-    else:
-        # Handle the case where there is no valid depth information in the region
-        return None
-
-# Define camera parameters (example values, replace with your actual camera parameters)
-focal_length = 3.0  # Focal length in millimeters
-camera_center_x = 320  # Optical center's x-coordinate in the image frame
-camera_center_y = 240  # Optical center's y-coordinate in the image frame
-
-def process_image(frame, camera_center_x, camera_center_y, focal_length):
-    depth_map = calculate_depth_map(frame)
-
-    results = model(frame)
-
-    detected_objects = {}
-    for r in results:
-        boxes = r.boxes
-        for box in boxes:
-            x1, y1, x2, y2 = box.xyxy[0]
-            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-            w, h = x2 - x1, y2 - y1
-            conf = math.ceil((box.conf[0] * 100)) / 100
-            cls = int(box.cls[0])
-            if 0 <= cls < len(classNames):
-                object_class = classNames[cls]
-            else:
-                object_class = "Unknown"
-
-            # Calculate the percentage of the image size covered by the bounding box
-            image_area = frame.shape[0] * frame.shape[1]
-            box_area = w * h
-            coverage_percentage = (box_area / image_area) * 100
-
-            
-
-            # Check if we have detected objects of this class before
-            if object_class not in detected_objects_per_class:
-                detected_objects_per_class[object_class] = 1
-            else:
-                detected_objects_per_class[object_class] += 1
-
-            # Assign a unique label to this object instance within the class
-            object_name = f"{object_class}{detected_objects_per_class[object_class]}"
-
-            # Calculate the distance using the depth map
-            distance = calculate_distance(x1, y1, x2, y2, depth_map)
-
-            # Calculate horizontal and vertical deviations in degrees
-            horizontal_deviation = math.degrees(math.atan((x1 + w/2 - camera_center_x) / focal_length))
-            vertical_deviation = math.degrees(math.atan((y1 + h/2 - camera_center_y) / focal_length))
-
-            detected_objects[object_name] = {
-                'distance': distance,
-                'horizontal_deviation': horizontal_deviation,
-                'vertical_deviation': vertical_deviation
-            }
-            # Choose the color based on coverage percentage
-            if coverage_percentage > 50:
-                color = (0, 0, 255)  # Red
-                cv2.putText(frame, f"{conf:.2f}) distance: {distance:.2f} :Too Near", (x1, y1 - 10), cv2.FONT_HERSHEY_TRIPLEX, 0.5, color, 2)
-            else:
-                color = (0, 255, 0)  # Green
-            # Draw small dots at x1, y1, x2, y2
-            cv2.circle(frame, (x1, y1), 3, color, -1)
-            cv2.circle(frame, (x2, y2), 3, color, -1)  
-            # Draw horizontal lines based on distance
-            line_color = (0, 255, 0)  # Green color
-            line_thickness = 2
-
-            for y in range(frame.shape[0] - 1, camera_center_y, -10):
-                if y >= 0 and y < len(depth_map):
-                    # Calculate the depth at the line's position
-                    line_depth = depth_map[y][camera_center_x]
-
-                    # Calculate the line length based on depth (adjust as needed)
-                    line_length = int(line_depth * 2)  # You may need to adjust the scaling factor
-
-                    # Calculate the starting and ending points of the line
-                    start_point = (camera_center_x - line_length // 2, y)
-                    end_point = (camera_center_x + line_length // 2, y)
-
-                    cv2.line(frame, start_point, end_point, line_color, line_thickness)
-            #Add "VizuoSense" text to the top left corner
-            
-            text = "VS engine running..."
-            font = cv2.FONT_HERSHEY_TRIPLEX
-            font_scale = 0.6
-            font_color = (64, 64, 64)  # White color
-            cv2.putText(frame, text, (10, 30), font, font_scale, font_color, 1)
-            
-            
-
-    return frame, detected_objects
-
-# Define a dictionary to keep track of detected objects per class
-detected_objects_per_class = {}
-
-# Initialize a dictionary to store processed image data
-processed_frames = {}
-
-# Initialize a variable to keep track of time
-start_time = time()
-
-# Specify the time interval (in seconds) for processing frames
-processing_interval = 10
-
-# Initialize a frame count
-frame_count = 0
-
-# Specify the maximum number of frames to process (optional)
-max_frames_to_process = 10  # Change this to process a specific number of frames
-
-# Initialize the VideoCapture object (0 for default camera, or provide the video file path)
-cap = cv2.VideoCapture(0)  # Change the argument if using a different video source
-
-while True:
-    ret, frame = cap.read()
-
-    if not ret:
-        break
-
-    frame_count += 1
-
-    # Process the frame
-    processed_image, detected_objects = process_image(frame, camera_center_x, camera_center_y, focal_length)
-
-    # Save the processed image with bounding boxes
-    output_image_path = os.path.join(output_directory, f"frame_{frame_count}.jpg")
-    cv2.imwrite(output_image_path, processed_image)
-
-    # Calculate elapsed time
-    elapsed_time = time() - start_time
-
-    # If the elapsed time exceeds the processing interval, store the data in the dictionary
-    if elapsed_time >= processing_interval:
-        processed_frames[frame_count] = {
-            'frame_count': frame_count,
-            'detected_objects': detected_objects
-        }
-
-        # Reset the timer
-        start_time = time()
-
-        # When saving the JSON data, use the custom encoder
-        json_data = json.dumps(processed_frames, indent=4, cls=NumpyEncoder)
-        output_json_path = f"C:/Users/Admin/Documents/processed_frames_{frame_count}.json"
-        with open(output_json_path, "w") as json_file:
-            json_file.write(json_data)
-        print(f"Success! JSON data saved to {output_json_path}")
-
-    # If you want to break the loop after processing a specific number of frames, you can add a condition here
-    if max_frames_to_process and frame_count >= max_frames_to_process:
-        break
-
-# Release the VideoCapture and close any open windows
-cap.release()
-
+    cap = cv2.VideoCapture(0)
+    processing_interval = 10
+    max_frames_to_process = 10
+    frame_analyzer = FrameAnalyzer(output_directory, processing_interval, max_frames_to_process)
+    frame_analyzer.analyze_frames(cap, frame_processor)
+    cap.release()
