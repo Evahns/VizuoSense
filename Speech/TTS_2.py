a327f6922feb2c44cc29204a37a4c7d35d16570a
Evahns <ngundievans2@gmail.com>
2023-11-10 21:05:51 +0300

    revert modification

--- Speech/TTS_2.py
+++ Speech/TTS_2.py
@@ -2,7 +2,6 @@ import  pyttsx3
 import json, logging as log
 log.basicConfig(level=log.INFO)
 
-#Replace the following with the path to your text file
 text_path = "D:/vizuosense_mine/STT/Resources/text_file.txt"
 def text_to_speech(path):
     with open(path, "r+") as to_read:
@@ -11,8 +10,9 @@ def text_to_speech(path):
         engine = pyttsx3.init()
         engine.setProperty('rate', 150)
         log.info(read)
-        log.info(engine.getProperty('rate'))
+        print(engine.getProperty('rate'))
         engine.say(read)
         engine.runAndWait()
+
 
 text_to_speech(text_path)
